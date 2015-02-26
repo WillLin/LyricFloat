06187d22c1c05adbac07b7cfa217a259c90533f7
PavelEE <zaytsev.pavel.a@gmail.com>
2015-02-25 22:29:00 -0800

    tag fucking cloud

--- submit.php
+++ submit.php
@@ -1,24 +1,28 @@
 <html>
-	  <head>
-	    <title>LyricFloat</title>
-	  </head>
-	
-	  <body>
-	  <h1>LyricFloat</h1>
-	    
-	   <!-- <p>this is where cloud will go</p>*/ -->
-	   <a href="page2.php" > <?php  echo $_GET["song"];  ?> </a>
+	<head>
+		<title>LyricFloat</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
 
-	   <br><br><br><br><br><br><br><br><br><br>
-	    <form action="submit.php" method="get">
-			<input type="text" name="song" placeholder="Input text here" size="35" >
-			<br>
-			<input type="button" value="Add to Cloud">
-			<input type="button" value="Share">
-			<input type="submit" value="Submit">
-	    </form>
+	<body>
+	<!-- <h1>LyricFloat</h1> -->
+	<div id="logo">
+		<a href="./"><img src="images/lyricfloat_sm.png" alt="LyricFloat" /></a>
+	</div>
+
+	<!-- <p>this is where cloud will go</p>*/ -->
+	<a href="page2.php" > <?php  echo $_GET["song"];  ?> </a>
+
+	<br><br><br><br><br><br><br><br><br><br>
+	<form action="submit.php" method="get">
+		<input type="text" name="song" placeholder="Input text here" size="35" >
+		<br>
+		<input type="button" value="Add to Cloud">
+		<input type="button" value="Share">
+		<input type="submit" value="Submit">
+	</form>
 
 
-	  </body>
+	</body>
 	
 </html>
