5aa56b46d2f40234968126e2951cb70a8e8d415a
William Lin <linwilli@usc.edu>
2015-02-25 20:48:46 -0800

    Centered, incomplete back button

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <html>
-	  <head>
-	    <title>LyricFloat</title>
-	    <link rel="stylesheet" type="text/css" href="css/styles.css">
-	  </head>
+	<head>
+		<title>LyricFloat</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
 	
-	  <body>
-	  <!-- <h1>LyricFloat</h1> -->
-	  <div id="logo">
-	  	<img src="images/lyricfloat.png" alt="LyricFloat" />
-	  </div>
-	    
-	   <!-- <p>this is where cloud will go</p>*/ -->
-	   
-	   <br><br><br><br><br><br><br><br><br><br>
-	    <form action="submit.php" method="get">
+	<body>
+	<!-- <h1>LyricFloat</h1> -->
+	<div id="logo">
+		<a href="./"><img src="images/lyricfloat.png" alt="LyricFloat" /></a>
+	</div>
+
+	<!-- <p>this is where cloud will go</p>*/ -->
+
+	<br><br><br><br><br><br><br><br><br><br>
+	<div id="inputarea">
+		<form action="submit.php" method="get">
 			<input type="text" name="song" placeholder="Input text here" size="35" >
 			<br>
 			<input type="submit" value="Submit">
-	    </form>
+		</form>
+	</div>
 
 
-	  </body>
+	</body>
 	
 </html>
--- page2.php
+++ page2.php
@@ -1,26 +1,31 @@
 <html>
-	  <head>
-	    <title>LyricFloat</title>
-	    <link rel="stylesheet" type="text/css" href="css/styles.css">
-	  </head>
-	
-	  <body>
-	  <h1>LyricFloat - Page 2 </h1>
-	  <h2>Search World</h2>
-	  <p>Song Title.............................Frequency</p>
-	  <p>Song Title.............................Frequency</p>
-	  <p>Song Title.............................Frequency</p>
-	    
-	   <!--  Chart lyrics
-	   <p>this is where cloud will go</p>*/ 
-	   <a href="page2.php" > ?php  echo $_GET["song"];  ?> </a>
-	   -->
+	<head>
+		<title>LyricFloat</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
 
-	   <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-	    <form action="index.php">
-   			 <input type="submit" value="Back">
+	<body>
+		<!-- <h1>LyricFloat - Page 2 </h1> -->
+		<div id="logo">
+			<a href="./"><img src="images/lyricfloat_sm.png" alt="LyricFloat" /></a>
+		</div>
+
+		<h2>Search World</h2>
+		<p>Song Title.............................Frequency</p>
+		<p>Song Title.............................Frequency</p>
+		<p>Song Title.............................Frequency</p>
+
+		<!--  Chart lyrics
+		<p>this is where cloud will go</p>*/ 
+		<a href="page2.php" > ?php  echo $_GET["song"];  ?> </a>
+		-->
+
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<form action="submit.php" method="get">
+			<input type="hidden" name="song" value="testttt" />
+			<input type="submit" value="Back">
 		</form>
 
-	  </body>
-	
+		</body>
+
 </html>
