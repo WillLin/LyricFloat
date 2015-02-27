c3f6c272ee8f74064267171156f3db4e7c500e35
Weidi Cui <weidicui@usc.edu>
2015-02-26 17:59:12 -0800

    word and song class created

---
+++ song.php
@@ -0,0 +1,45 @@
+<?php
+	/**
+	* 
+	*/
+	require 'word.php';
+	class Song
+	{
+		private var $name;
+		private var $artist;
+		private var $wordlist=array();
+		function __construct($name,$artist)
+		{
+			$this->name=$name;
+			$this->artist=$artist;
+		}
+		function parseLyrics($lyrics)
+		{
+			$lines=explode("\n",$lyrics);
+			foreach($lines as $value){
+				$words=explode(" ",$value);
+				foreach($words as $word){
+					$word=trim($word,"!.;:\",\r\0\x0B");
+					$w=new Word($word);
+					if(!in_array($w, $wordlist)){
+						array_push($wordlist, $w);
+					}
+				}
+			}
+			foreach ($wordlist as $word) {
+				$word->parseLyrics($lyrics);
+			}
+		}
+		function getName()
+		{
+			return $this->name;
+		}
+		function getArtist()
+		{
+			return $this->artist;
+		}
+		function getWordList(){
+			return $this->wordlist;
+		}
+	}
+?>
---
+++ word.php
@@ -0,0 +1,30 @@
+<?php
+	/**
+	* 
+	*/
+	class Word
+	{
+		private var $wordName;
+		private var $frequency;
+		
+		function __construct($name)
+		{
+			$this->wordName=$name;
+			$this->frequency=0;
+		}
+
+		function parseLyrics($lyrics)
+		{
+			$this->frequency=substr_count($lyrics, $this->wordName);
+		}
+
+		function getName()
+		{
+			return $this->wordName;
+		}
+		function getFrequency()
+		{
+			return $this->frequency;
+		}
+	}
+?>
