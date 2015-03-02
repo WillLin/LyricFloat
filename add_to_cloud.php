39b7ef65ac00a1e127f47389cebd7b127e922770
PavelEE <zaytsev.pavel.a@gmail.com>
2015-03-02 01:24:18 -0800

    add to cloud normal

--- add_to_cloud.php
+++ add_to_cloud.php
@@ -1,37 +1,46 @@
 <?php
-session_start();
+//session_start();
 include('api_connection_setup.php');
 
-$transferred_array = $_SESSION['allWords'];
 
-$artist = $_GET['artist'];
-				$songs_array = getAllLyrics($artist);
+function add_to_cloud($artist)
+{
+	$transferred_array = array();
+	$transferred_array = $_SESSION['allWords'];
 
-				$result = array();
-				$result = $songs_array[0]->getWordList();
-				for ($i = 1; $i < count($songs_array); $i++)
-				{
-					$result = array_merge($result, $songs_array[$i]->getWordList());
+	//$artist = $_GET['artist'];
+					$songs_array = getAllLyrics($artist);
 
-				}
+					$result = array();
+					$result = $songs_array[0]->getWordList();
+					for ($i = 1; $i < count($songs_array); $i++)
+					{
+						$result = array_merge($result, $songs_array[$i]->getWordList());
 
-				$newarraycount = 0;
-				$newarray = array();
-
-				foreach ($result as $key => $value) 
-				{
-					while ($value > 1) {
-						$newarray[$newarraycount] = $key;
-						$newarraycount++;
-						$value--;
 					}
 
+					$newarraycount = 0;
+					$newarray = array();
 
-					//echo "$key .  $value <br>";
-				}
+					foreach ($result as $key => $value) 
+					{
+						while ($value > 1) {
+							$newarray[$newarraycount] = $key;
+							$newarraycount++;
+							$value--;
+						}
 
-				$transferred_array = array_merge($transferred_array, $newarray);
-				$_SESSION['allWords'] = $transferred_array;
+
+						//echo "$key .  $value <br>";
+					}
+
+					$merged_array = array_merge($transferred_array, $newarray);
+					$_SESSION['allWords'] = $merged_array;
+
+					return $merged_array;
+
+}
+				//header('Location: submit2.php');
 
 ?>
 
