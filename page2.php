e3a8a8401c52ca97b3c35414ae6994c18b894720
PavelEE <zaytsev.pavel.a@gmail.com>
2015-03-02 02:39:05 -0800

    song list

--- page2.php
+++ page2.php
@@ -1,3 +1,44 @@
+<?php
+
+include('songlist.php');
+session_start();
+function countFreq($word)
+{
+
+	$songs = array();
+	$songs = $_SESSION['songsArray'];
+
+	$data_array = array();
+
+
+	/*for ($i = 0; $i < count($songs); $i++)
+	{
+			foreach($songs[$i]->getWordList() as $key=>$value)
+			{
+					if ($word == $key)
+					{		
+							//$data_array[$songs[$i]] = $value;	
+							echo $songs[$i]->getName() . $value . '<br>';
+					}
+
+
+
+			}
+
+		//	return $data_array;
+	}*/
+
+
+	$lists=new Songlist($word);
+	$lists->setList($songs);
+	$songlist=$lists->getList();
+	foreach ($songlist as $songName => $frequency) {
+		echo $songName . " ................................ "  . $frequency  . '<br>';
+	}
+}
+?>
+
+
 <html>
 	<head>
 		<title>LyricFloat</title>
@@ -13,11 +54,17 @@
 		<div id="content">
 			<h2 id="searchword"><?php echo $_GET['word']; ?></h2>
 			<div id="songlist">
-				<p><a href="songlyrics.php?artist=test&amp;word=testtt&amp;song=Song+Title+1">Song Title 1</a>.............................Frequency</p>
-				<p>Song Title.............................Frequency</p>
-				<p>Song Title.............................Frequency</p>
+				<!-- <p><a href="songlyrics.php?artist=test&amp;word=testtt&amp;song=Song+Title+1">Song Title 1</a>.............................<?php echo countFreq($_GET['word'])?></p>
+				<p>Song Title.............................<?php echo countFreq($_GET['word'])?></p>
+				<p>Song Title.............................<?php echo countFreq($_GET['word'])?></p> -->
 			</div>
 
+				<?php 
+
+
+				countFreq($_GET['word']);
+
+				?>
 			<!--  Chart lyrics
 			<p>this is where cloud will go</p>*/ 
 			<a href="page2.php" > ?php  echo $_GET["song"];  ?> </a>
